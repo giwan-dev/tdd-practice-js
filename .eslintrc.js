c577a4b2780116133c35245fd58044ca9e261a04
박기완 <zprime0920@gmail.com>
2018-07-15 20:51:51 +0900

    곱한 값이 새로운 Dollar를 리턴하도록 테스트 작성.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
-        "import/prefer-default-export": "off"
+        "import/prefer-default-export": "off",
+        "no-underscore-dangle": [0, { "allow": true }]
     }
 };

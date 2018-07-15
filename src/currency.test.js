e784c4105ada6cd19d45b8fb51c0c8d58e527c1b
박기완 <zprime0920@gmail.com>
2018-07-15 20:30:49 +0900

    Chapter2: 타락한 객체 (#2)
    * more test cases
    
    * make it work

--- src/currency.test.js
+++ src/currency.test.js
@@ -2,6 +2,8 @@ import { Dollar } from './currency';
 
 test('$5 X 2 = $10', () => {
   const five = new Dollar(5);
-  five.times(2);
-  expect(Number(five.amount)).toBe(10);
+  let product = five.times(2);
+  expect(product).toBe(10);
+  product = five.times(3);
+  expect(product).toBe(15);
 });

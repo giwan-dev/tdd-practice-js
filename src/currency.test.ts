878dd0e4e01911ebae46b44a9cd19081a97a0da8
박기완 <zprime0920@gmail.com>
2018-07-22 21:53:33 +0900

    Refactor: 테스트 코드 간소화

--- src/currency.test.ts
+++ src/currency.test.ts
@@ -9,10 +9,7 @@ test('달러 곱셈 테스트', () => {
 test('동일 확인', () => {
   expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
   expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-  expect(new Franc(5).equals(new Franc(5))).toBe(true);
-  expect(new Franc(5).equals(new Franc(6))).toBe(false);
   expect(new Dollar(5).equals(new Franc(5))).toBe(false);
-  expect(new Franc(5).equals(new Dollar(5))).toBe(false);
 });
 
 test('프랑 곱셈 테스트', () => {

2c38ed1c5d5865f2e11211917d3dde70cd394860
박기완 <zprime0920@gmail.com>
2018-07-22 16:27:06 +0900

    Chapter8: 객체 만들기 (#8)
    * Refactor: 테스트 코드에서 Dollar, Franc 클래스 decoupling
    
    * Refactor: times 함수 Money class로 이동

--- src/currency.js
+++ src/currency.js
@@ -3,19 +3,31 @@ export class Money {
     this._amount = amount; // private member convention. 실제로 제한되지 않음
   }
 
+  static dollar(amount) {
+    return new Dollar(amount);
+  }
+
+  static franc(amount) {
+    return new Franc(amount);
+  }
+
+  times(multiplier) {
+    if (this instanceof Dollar) {
+      return new Dollar(this._amount * multiplier);
+    }
+    if (this instanceof Franc) {
+      return new Franc(this._amount * multiplier);
+    }
+    return null;
+  }
+
   equals(object) {
     return this._amount === object._amount && this instanceof Dollar === object instanceof Dollar;
   }
 }
 
 export class Dollar extends Money {
-  times(multiplier) {
-    return new Dollar(this._amount * multiplier);
-  }
 }
 
 export class Franc extends Money {
-  times(multiplier) {
-    return new Franc(this._amount * multiplier);
-  }
 }

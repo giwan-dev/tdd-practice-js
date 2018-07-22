db85fb4794afbe9eee72de1af6ff97cd2d263f02
박기완 <zprime0920@gmail.com>
2018-07-22 14:07:54 +0900

    Refactor: Dollar와 Franc의 equals 메서드 제거.
    제거를 위해 Franc equals 테스트를 추가했다.

--- src/currency.js
+++ src/currency.js
@@ -2,24 +2,20 @@ export class Money {
   constructor(amount) {
     this._amount = amount; // private member convention. 실제로 제한되지 않음
   }
+
+  equals(object) {
+    return this._amount === object._amount;
+  }
 }
 
 export class Dollar extends Money {
   times(multiplier) {
     return new Dollar(this._amount * multiplier);
   }
-
-  equals(object) {
-    return this._amount === object._amount;
-  }
 }
 
 export class Franc extends Money {
   times(multiplier) {
     return new Franc(this._amount * multiplier);
-  }
-
-  equals(object) {
-    return this._amount === object._amount;
   }
 }

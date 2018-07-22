1340fabcd22630087657f058fcd53d54bcbe3ebd
박기완 <zprime0920@gmail.com>
2018-07-22 16:42:35 +0900

    Chapter9: 우리가 사는 시간 (#9)
    * Feature: 통화 단위 추가
    
    * Refactor: currency를 Money 클래스로 이동
    
    * lint

--- src/currency.js
+++ src/currency.js
@@ -1,28 +1,33 @@
 export class Money {
-  constructor(amount) {
+  constructor(amount, currency) {
     this._amount = amount; // private member convention. 실제로 제한되지 않음
+    this._currency = currency;
   }
 
   static dollar(amount) {
-    return new Dollar(amount);
+    return new Dollar(amount, 'USD');
   }
 
   static franc(amount) {
-    return new Franc(amount);
+    return new Franc(amount, 'CHF');
   }
 
   times(multiplier) {
     if (this instanceof Dollar) {
-      return new Dollar(this._amount * multiplier);
+      return new Dollar(this._amount * multiplier, 'USD');
     }
     if (this instanceof Franc) {
-      return new Franc(this._amount * multiplier);
+      return new Franc(this._amount * multiplier, 'CHF');
     }
     return null;
   }
 
   equals(object) {
     return this._amount === object._amount && this instanceof Dollar === object instanceof Dollar;
+  }
+
+  currency() {
+    return this._currency;
   }
 }
 
--- src/currency.test.js
+++ src/currency.test.js
@@ -1,4 +1,4 @@
-import { Dollar, Franc, Money } from './currency';
+import { Money } from './currency';
 
 test('Test multiplication', () => {
   const five = Money.dollar(5);
@@ -18,4 +18,9 @@ test('Test equality', () => {
   expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
   expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
   expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
+});
+
+test('TestCurrency', () => {
+  expect(Money.dollar(1).currency()).toBe('USD');
+  expect(Money.franc(1).currency()).toBe('CHF');
 });

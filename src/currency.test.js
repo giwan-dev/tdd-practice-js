81791aa52b944021a181997d0155c3c60143d136
박기완 <zprime0920@gmail.com>
2018-07-22 13:55:51 +0900

    Chapter5: 솔직히 말하자면 (#5)
    * Add Franc Class
    
    * Fix: 누락된 수정 내역 반영

--- src/currency.test.js
+++ src/currency.test.js
@@ -1,9 +1,15 @@
-import { Dollar } from './currency';
+import { Dollar, Franc } from './currency';
 
 test('Test multiplication', () => {
   const five = new Dollar(5);
   expect(five.times(2)).toEqual(new Dollar(10));
   expect(five.times(3)).toEqual(new Dollar(15));
+});
+
+test('Test franc multiplication', () => {
+  const five = new Franc(5);
+  expect(five.times(2)).toEqual(new Franc(10));
+  expect(five.times(3)).toEqual(new Franc(15));
 });
 
 test('Test equality', () => {

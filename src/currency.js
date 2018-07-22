e17cce5c3bd66d98c06c4ad11d3c49a74d9a4b95
박기완 <zprime0920@gmail.com>
2018-07-22 13:50:58 +0900

    Add Franc Class

--- src/currency.js
+++ src/currency.js
@@ -11,3 +11,17 @@ export class Dollar {
     return this._amount === object._amount;
   }
 }
+
+export class Franc {
+  constructor(amount) {
+    this._amount = amount; // private member convention. 실제로 제한되지 않음
+  }
+
+  times(multiplier) {
+    return new Dollar(this._amount * multiplier);
+  }
+
+  equals(object) {
+    return this._amount === object._amount;
+  }
+}

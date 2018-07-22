f3aa6d8c0f0414436ec68987a9f2abc109c62617
박기완 <zprime0920@gmail.com>
2018-07-22 22:11:31 +0900

    Refactor: Dollar, Franc 클래스 제거

--- src/currency.test.ts
+++ src/currency.test.ts
@@ -1,13 +1,19 @@
-import { Dollar } from './dollar';
-import { Franc } from './franc';
+import { Money } from './money';
 
 test('곱셈 테스트', () => {
-  const five = new Dollar(5);
+  const five = Money.dollar(5);
   expect(five.times(2)).toBe(10);
 });
 
 test('동일 확인', () => {
-  expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
-  expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-  expect(new Dollar(5).equals(new Franc(5))).toBe(false);
+  const five = Money.dollar(5);
+
+  expect(five.equals(Money.dollar(5))).toBe(true);
+  expect(five.equals(Money.dollar(6))).toBe(false);
+  expect(five.equals(Money.franc(5))).toBe(false);
+});
+
+test('통화 확인', () => {
+  const five = Money.dollar(5);
+  expect(five.currency).toBe('USD');
 });
--- src/money.ts
+++ src/money.ts
@@ -1,14 +1,22 @@
-import { Dollar } from './dollar';
-
 export class Money {
+  public static dollar(amount: number): Money {
+    return new Money(amount, 'USD');
+  }
+
+  public static franc(amount: number): Money {
+    return new Money(amount, 'CHF');
+  }
+
+  public currency: string;
   protected amount: number;
 
-  constructor(amount: number) {
+  constructor(amount: number, currency: string) {
     this.amount = amount;
+    this.currency = currency;
   }
 
   public equals(money: Money): boolean {
-    return this.amount === money.amount && this instanceof Dollar === money instanceof Dollar;
+    return this.amount === money.amount && this.currency === money.currency;
   }
 
   public times(multiplier: number): number {
